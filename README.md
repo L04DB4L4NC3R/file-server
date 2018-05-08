2bf677f0b91cc2f1bddcbb0c61685d59baee86f7
angad <angadsharma1016@gmail.com>
2018-05-08 20:00:04 +0530

    install.sh done

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # file-server
 
 
-Serving express-auth-generator on https://fileserve.herokuapp.com/download/expressauthgenerator
+## Serving express-auth-generator on https://fileserve.herokuapp.com/download/express_auth_generator
+
+```
+$ curl -L https://fileserve.herokuapp.com/download/express_auth_generator | bash
+```
--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get("/download/expressauthgenerator",(req,res)=>{
+app.get("/download/express_auth_generator",(req,res)=>{
+
+    var file = __dirname + "/files/install";
+    res.download(file);
+});
+
+app.get("/download/getauthgen",(req,res)=>{
 
     var file = __dirname + "/files/authgen";
     res.download(file);
 });
+
 
 app.listen(process.env.PORT || 3000,()=>console.log("Listening..."));
