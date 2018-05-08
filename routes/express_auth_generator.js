4615ff4e9ebc7bd046d7e671a314f4e734cbfd14
angad <angadsharma1016@gmail.com>
2018-05-08 20:31:40 +0530

    cleaning up EAS

--- routes/express_auth_generator.js
+++ routes/express_auth_generator.js
@@ -2,13 +2,13 @@ const app = require("express").Router();
 
 app.get("/download/express_auth_generator",(req,res)=>{
 
-    var file = __dirname + "/files/install";
+    var file = (__dirname).split("routes")[0] + "/files/EAS/install";
     res.download(file);
 });
 
 app.get("/download/getauthgen",(req,res)=>{
 
-    var file = __dirname + "/files/authgen";
+    var file = (__dirname).split("routes")[0] + "/files/EAS/authgen";
     res.download(file);
 });
 
